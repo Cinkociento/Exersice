e4ec9487cd684c772a43f6c01ba81e292537dee0
YellowBlack <potolop@interia.pl>
2024-09-30 21:54:57 +0200

    I made a skeleton of the project, then create protection against destroying the program

---
+++ exersice4.py
@@ -0,0 +1,27 @@
+# popros uzytkownika o dlugosc jendego i drugiego boku prostokata
+# oblicz pole i obwod prostokata (uzyj do tego dwie funckje)
+# wypisz pole i obwod prostokata
+# zabezpiecz program tak zeby uzytkownik nie mogl go popsuc'
+def obwod_prostokata(a, b):
+  obwod = a + b + a + b
+  return obwod
+def pole_prostokata(a, b):
+  pole = a * b
+  return pole
+
+# try:
+  
+# except:
+
+
+b1 = int(input("---------------Podaj pierwszy bok prostokąta: ---------------\nLiczbe wpisz tutaj: "))
+b2 = int(input("---------------Podaj drugi bok prostokąta: ---------------\nLiczbe wpisz tutaj: "))
+
+b1_finish = obwod_prostokata(b1, b2)
+b2_finish = obwod_prostokata(b1, b2)
+
+print(f"\n---------------Obwód twojego prostokąta to: {b1_finish}---------------") 
+# print(b1_finish)
+
+print(f"-----------Natomiast pole twojego prostokąta to: {b2_finish}-----------")
+# print(b2_finish)
---
+++ exersice5.py
@@ -0,0 +1,2 @@
+# Napisz funkcję, która przyjmuje liste zawierającą liczby i zwraca nową liste z tymi 
+# samymi liczbami posortowanymi w porządku rosnącym lub malejacym, w zaleznosci od argumentu podanego do funckji.
