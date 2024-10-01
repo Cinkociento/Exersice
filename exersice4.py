d030ff4425e15a0d8b4bf3bee54f786dec209419
YellowBlack <potolop@interia.pl>
2024-10-01 15:57:08 +0200

    safely and complete tasks 5 and 4

--- exersice4.py
+++ exersice4.py
@@ -2,6 +2,9 @@
 # oblicz pole i obwod prostokata (uzyj do tego dwie funckje)
 # wypisz pole i obwod prostokata
 # zabezpiecz program tak zeby uzytkownik nie mogl go popsuc'
+import sys
+import time
+
 def obwod_prostokata(a, b):
   obwod = a + b + a + b
   return obwod
@@ -9,10 +12,13 @@ def pole_prostokata(a, b):
   pole = a * b
   return pole
 
-# try:
-  
-# except:
-
+try:
+  b1 = int(input("---------------Podaj pierwszy bok prostokąta: ---------------\nLiczbe wpisz tutaj: "))
+  b2 = int(input("---------------Podaj drugi bok prostokąta: ---------------\nLiczbe wpisz tutaj: "))
+except ValueError:
+  print("ERROR! - Wpisz liczbe!")
+  time.sleep(2)
+  sys.exit(1)
 
 b1 = int(input("---------------Podaj pierwszy bok prostokąta: ---------------\nLiczbe wpisz tutaj: "))
 b2 = int(input("---------------Podaj drugi bok prostokąta: ---------------\nLiczbe wpisz tutaj: "))
--- exersice5.py
+++ exersice5.py
@@ -1,9 +1,17 @@
 # Napisz funkcję, która przyjmuje liste zawierającą liczby i zwraca nową liste z tymi 
 # samymi liczbami posortowanymi w porządku rosnącym lub malejacym, w zaleznosci od argumentu podanego do funckji.
 import sys
+import time
 
 def pobrana_lista():
   lista = []
+  try:
+      pobierz = int(input("Podaj liczby do listy: "))
+  except ValueError:
+      print("ERROR! - Wpisz liczbe!")
+      time.sleep(2)
+      sys.exit(1)
+
   for i in range(5):
     pobierz = int(input("Podaj liczby do listy: "))
     lista.append(pobierz)
@@ -15,6 +23,8 @@ def pobrana_lista():
     lista.sort(reverse=True)
     return lista
   else:
+    print("ERROR! - Wybrano nieprawidłowa! fraze Wybierz pomiedzy sortowaniem malejacym lub rosnacym")
+    time.sleep(2)
     sys.exit(1)
 
 x = pobrana_lista()
